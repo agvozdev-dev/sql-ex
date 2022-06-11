b4b8204f9ba59bef2ad380710c5b9e1e06632315
Alexander Gvozdev <agvozdev.dev@gmail.com>
2022-06-11 21:53:56 +0700

    2 task

--- computer-firm/1.sql
+++ computer-firm/1.sql
@@ -1,6 +1,7 @@
 /*
 Find the model number, speed and hard drive capacity for all the PCs with prices below $500.
 Result set: model, speed, hd.
+Result set: model, speed, hd.
 */
 
 select product.model, pc.speed, pc.hd
---
+++ computer-firm/2.sql
@@ -0,0 +1,8 @@
+/*
+List all printer makers. 
+Result set: maker.
+*/
+
+select distinct maker
+from product
+where type = 'Printer'
