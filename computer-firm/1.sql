16ca1e8b21318da5718b11664ec8f17ef32e5647
Alexander Gvozdev <agvozdev.dev@gmail.com>
2022-06-11 21:52:03 +0700

    1 task

--- computer-firm/1.sql
+++ computer-firm/1.sql
@@ -1,3 +1,8 @@
+/*
+Find the model number, speed and hard drive capacity for all the PCs with prices below $500.
+Result set: model, speed, hd.
+*/
+
 select product.model, pc.speed, pc.hd
 from product
 join pc on product.model = pc.model
